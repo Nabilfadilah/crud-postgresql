26a27c50157c91bcc8fef0f951b04699e3e47b11
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-04 09:46:55 +0700

    Menambah controller create dan store untuk add post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,36 @@ use App\Models\post;
 
 class PostController extends Controller
 {
+    // view page
     public function index(Request $request)
     {
         $posts = post::paginate(5);
 
         return view("posts.index", compact('posts'));
+    }
+
+    // add 
+    public function create(Request $request)
+    {
+        return view("posts.create");
+    }
+
+    // store post add
+    public function store(Request $request)
+    {
+        $request->validate([
+            "title" => "required",
+            "body" => "required"
+        ]);
+
+        Post::create([
+            "title" => $request->title,
+            "body" => $request->body
+        ]);
+
+        return redirect()->route("post.index")->with("success", "Post created.");
+
+        // console
+        // dd($request->all());
     }
 }
