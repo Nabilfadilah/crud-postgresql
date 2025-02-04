5dc2b28f11333d7c5974c762335adb9286522845
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-04 09:58:27 +0700

    Memberikan layout pada index post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,62 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('posts.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11 CRUD - PostgreSQL</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@section('content')
 
-</head>
-
-<body>
-
-    <div class="container pt-5">
-        <div class="card">
-            <div class="card-header">
-                <div class="row">
-                    <div class="col-md-9">
-                        <h4>Post</h4>
-                    </div>
-                    <div class="col-md-3 text-end">
-                        <a href="{{ route('post.create') }}" class="btn btn-success">Create Post</a>
-                    </div>
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-9">
+                    <h4>Post</h4>
+                </div>
+                <div class="col-md-3 text-end">
+                    <a href="{{ route('post.create') }}" class="btn btn-success">Create Post</a>
                 </div>
             </div>
-            <div class="card-body">
+        </div>
+        <div class="card-body">
 
-                @session('success')
-                    <div class="alert alert-success">{{ $value }}</div>
-                @endsession
+            @session('success')
+                <div class="alert alert-success">{{ $value }}</div>
+            @endsession
 
-                <table class="table table-bordered">
-                    <thead>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($posts as $post)
                         <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Body</th>
+                            <td>{{ $post->id }}</td>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->body }}</td>
+                            <td></td>
                         </tr>
-                    </thead>
-
-                    <tbody>
-                        @foreach ($posts as $post)
-                            <tr>
-                                <td>{{ $post->id }}</td>
-                                <td>{{ $post->title }}</td>
-                                <td>{{ $post->body }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
